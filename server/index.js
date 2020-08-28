f3a1b12650bc976f987aa436eca4a37b443f24c7
Vinny Nyamu <vincenyamu@gmail.com>
2020-08-27 22:22:53 -0400

    Enable runtime config with create-react-app-inner-buildpack

--- server/index.js
+++ server/index.js
@@ -4,19 +4,19 @@ const path = require('path');
 
 const port = process.env.PORT;
 
-app.get('/', (req, res, next) => {
-  res.setHeader('Content-Type', 'text/html');
-});
+// app.get('/', (req, res, next) => {
+//   res.setHeader('Content-Type', 'text/html');
+// });
 
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
   app.use(express.static('react-ui/build'));
-
-  // Handle React routing, return all requests to React app
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'react-ui', 'build', 'index.html'));
-  });
 }
+
+// Handle React routing, return all requests to React app
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './react-ui/build/index.html'));
+});
 
 app.listen(port, () => {
   console.log('Server started successfully');
