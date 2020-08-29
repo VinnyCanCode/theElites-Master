28fda191788ba0b89ecfb49625e2b45df76bcc81
Vinny Nyamu <vincenyamu@gmail.com>
2020-08-29 19:33:05 -0400

    Enable runtime config with create-react-app-inner-buildpack

--- client/src/index.js
+++ client/src/index.js
@@ -8,8 +8,7 @@ import thunk from 'redux-thunk';
 import App from './components/App';
 import reducers from './reducers';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
+const store = createStore(reducers, applyMiddleware(thunk));
 
 ReactDOM.render(
   <Provider store={store}>
